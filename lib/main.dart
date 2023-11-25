72eb2668637e7d9fddb91764455512ccc046e767
Chethiya2001 <chethiyabandaraherath@gmail.com>
2023-11-25 20:48:00 +0530

    step-03

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> commpactMap<E>([
+    E? Function(T?)? transform,
+  ]) =>
+      map(
+        transform ?? (e) => e,
+      ).where((e) => e != null).cast();
+}
+
+// void test() {
+//   final values = [1, 2, null, 3];
+//   final nonNullValues = values.commpactMap((e) {
+//     if (e!= null && e > 10) {
+//       return e;
+//     } else {
+//       return null;
+//     }
+//   });
+// }
 
 void main() {
   runApp(const MyApp());
@@ -23,20 +44,36 @@ class MyApp extends StatelessWidget {
   }
 }
 
+const url =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGvRdGiLBXC9-23P8lrAPQvOiIg-bR4E3pka7uv2Yosg&s';
+
 class MyHomePage extends HookWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final text = useState('');
-    useEffect(() {
-      controller.addListener(() {
-        text.value = controller.text;
-      });
-      return null;
-    }, [controller] //as a key
-        );
+    // final image = useFuture(
+    //   //grab image
+    //   NetworkAssetBundle(Uri.parse(url))
+    //       .load(url)
+    //       .then((data) => data.buffer.asUint8List())
+    //       .then(
+    //         (data) => Image.memory(data),
+    //       ),
+    // ); re-download
+    final future = useMemoized(
+      () =>
+          //grab image
+          NetworkAssetBundle(Uri.parse(url))
+              .load(url)
+              .then((data) => data.buffer.asUint8List())
+              .then(
+                (data) => Image.memory(data),
+              ),
+    );
+
+    final snapshot = useFuture(future);
+
     return Scaffold(
       appBar: AppBar(
         title: const Center(
@@ -46,12 +83,9 @@ class MyHomePage extends HookWidget {
       ),
       body: Column(
         children: [
-          TextField(controller: controller),
-          const SizedBox(
-            height: 20,
-          ),
-          Text('You types:${text.value}')
-        ],
+          // image.hasData ? image.data! : null, //re-download
+          snapshot.data
+        ].commpactMap().toList(),
       ),
     );
   }
